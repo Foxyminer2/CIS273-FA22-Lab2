83d23bbf263f2ff1a9a8d4f5480a291980c3d3bb
Aapple21 <aapplegate12@icloud.com>
2022-11-04 09:54:00 -0500

    Close to Done

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -54,17 +54,86 @@ public class Program
 
     private static bool Matches(char closing, char opening)
     {
-        throw new NotImplementedException();
+        if (opening == '(' && closing == ')')
+        {
+            return true;
+        }
+
+        if (opening == '{' && closing == '}')
+        {
+            return true;
+        }
+
+        if (opening == '[' && closing == ']')
+        {
+            return true;
+        }
+
+        if (opening =='<' && closing == '>')
+        {
+            return true;
+        }
+
+
+
+        return false;
     }
 
 
     public static double? Evaluate(string s)
     {
+
+        Stack<char> stack = new Stack<char>();
         // parse string into tokens
         string[] tokens = s.Split();
+        
 
+        
         // foreach token
         // if it's a number, push to stack
+        
+        foreach(var c in tokens)
+        {
+
+            int numericValue;
+            bool isNumber = int.TryParse(c, out numericValue);
+            if (isNumber == true)
+            {
+                char ch = Convert.ToChar(numericValue);
+                stack.Push(ch);
+                
+            }
+            //else if (isNumber == false && (stack.Count == 0 || stack.Count == 1))
+            //{
+                
+            //}
+            else
+            {
+                if (c == "+")
+                {
+
+                    stack.Push(Convert.ToChar(Convert.ToInt32(stack.Pop()) + Convert.ToInt32(stack.Pop())));
+                }
+                else if (c == "-")
+                {
+                    stack.Push(Convert.ToChar((Convert.ToInt32(stack.Pop()) * -1) + Convert.ToInt32(stack.Pop())));
+                }
+                else if (c == "*")
+                {
+
+                    stack.Push(Convert.ToChar(Convert.ToInt32(stack.Pop()) * Convert.ToInt32(stack.Pop())));
+                }
+                else if (c == "/")
+                {
+                    int x = Convert.ToInt32(stack.Pop());
+                    int y = Convert.ToInt32(stack.Pop());
+
+                    stack.Push(Convert.ToChar(y / x));
+                    
+                }
+            }
+
+        }
 
         // if it's a math operator, pop twice;
         // compute result;
@@ -72,7 +141,15 @@ public class Program
 
         // return top of stack (if the stack has 1 element)
 
-        return null;
+        if (stack.Count == 1)
+        {
+            int x = stack.Peek();
+            return x;
+        }
+        else
+        {
+            return null;
+        }
     }
 
 }
